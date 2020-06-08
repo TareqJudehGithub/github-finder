7e9c6f679a0880def16bc6a9d16b1f1c04ae702c
TareqJudehGithub <tareq.joudeh@gmail.com>
2020-06-09 02:05:25 +0300

    Created a Navbar component, and added a Navbar tag inside it with apropriate styling.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Navbar from "./components/layout/Navbar";
+
 import './App.css';
 
 class App extends React.Component {
+
   render() {
+   
     return (
+      
       <div className="App">
-      <h1>Hello From React</h1>
+        <Navbar /> 
       </div>
+      
+
     );
   }
   
